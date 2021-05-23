8c44f70f15e7feafeca5b2843306368c3a70f97b
Igouist <ursaliker@gmail.com>
2021-05-23 22:09:22 +0800

    準備紅藍方選手

--- BenchmarkDotnetTest/Program.cs
+++ BenchmarkDotnetTest/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace BenchmarkDotnetTest
 {
@@ -6,7 +10,37 @@ namespace BenchmarkDotnetTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<EmptyVSNewList>();
         }
+    }
+
+    /// <summary>
+    /// 測試用擂台
+    /// </summary>
+    public class EmptyVSNewList
+    {
+        // 紅方選手
+        [Benchmark]
+        public void Empty() => Enumerable.Empty<Foo>();
+
+        // 藍方選手
+        [Benchmark]
+        public void NewList()
+        {
+            new List<Foo>();
+        }
+    }
+
+    /// <summary>
+    /// 協力單位，用來當成串列的填充物
+    /// </summary>
+    public class Foo
+    {
+        public Guid Id { get; set; }
+        public string Bar1 { get; set; }
+        public string Bar2 { get; set; }
+        public string Bar3 { get; set; }
+        public string Bar4 { get; set; }
+        public string Bar5 { get; set; }
     }
 }
